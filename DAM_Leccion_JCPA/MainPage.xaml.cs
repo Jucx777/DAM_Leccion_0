15e555f4f50072e2d4a29814014f0a543611303b
jcpap <jcpap@JUCX>
2025-01-28 19:23:13 -0600

    Versión_3: Los dos tipos de Binding y el BindingContext

--- DAM_Leccion_JCPA/MainPage.xaml.cs
+++ DAM_Leccion_JCPA/MainPage.xaml.cs
@@ -26,11 +26,23 @@ namespace DAM_Leccion_JCPA
 
         public void Ejecutar()
         {
-            PersonaModel personaModel = new PersonaModel(); 
+            PersonaModel personaModel = new PersonaModel()
+            {
+                Nombre = "Hola aqui estoy",
+            };
 
-            personaModel.Nombre = "Hola aqui estoy";
+            BindingContext = personaModel.Nombre;
 
-            txtNombre.Text = personaModel.Nombre;
+           // Binding personaBinding = new Binding();
+
+            //personaBinding.Source = personaModel; //Origen
+            //personaBinding.Path = "Nombre"; //Ruta
+            //txtNombre.SetBinding(Entry.TextProperty, personaBinding); //Destino
+
+
+            //personaModel.Nombre = "Hola aqui estoy";
+
+            //txtNombre.Text = personaModel.Nombre;
         }
 
         private void btnAceptar_Clicked(object sender, EventArgs e)
